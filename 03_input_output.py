b645702928b4892fdd88e3b60599d1ad6b22fe53
Sneha Prajapati <snehaprajapati25@gmail.com>
2026-06-22 13:07:49 +0530

    input and output

---
+++ 03_input_output.py
@@ -0,0 +1,24 @@
+# input and output
+
+# output
+print("Hello, World!")
+print(12)
+name = 'sneha'
+age = "22"
+print(name, age)
+
+print("Hello my name is", name, "and my age is", age)
+
+#formatted string
+print(f"hello my name is {name} and my age is {age}")
+
+
+
+
+#input
+name = str(input("Enter your name: "))
+print(name)
+print(type(name))
+
+a = int(input("Enter a no: "))
+print(a)
