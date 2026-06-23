07c25078e9e2e06cfab29aa85a69a75fdd00f15f
Sneha Prajapati <snehaprajapati25@gmail.com>
2026-06-23 19:44:08 +0530

    operators

---
+++ 04_operators.py
@@ -0,0 +1,51 @@
+# opearators
+ 
+# airthemetic operators
+a = 5
+b = 25
+print(a+b) #addition
+print(b-a) #subtraction
+print(a*b) #multiplication
+print(b/a) #division  (5.0)
+print(b//a) #floor division  (5) conert into integer(6.5 -> 6)
+print(5**2) #exponentiation (5*5 = 25) 
+# print(5**3) #125 (5*5*5)
+print(10%3) #modulo (remainder) (1)
+
+
+#python follows BODMAS rule 
+
+
+## Assignment operators
+a = 20
+
+
+# compound assignment operators
+a = 20
+
+a = a+20 #a+=20
+a += 40
+a += 60
+print(a) #140
+
+#a +=
+#a -=
+#a *=
+#a /=, a //=, a **=, a%=
+
+
+
+# comparison operators
+#it will provide boolean results True or False
+# >, <, >=, <=, ==, !=
+
+print(ord("a")) #65
+print(ord("B")) #66
+print("A" > "B") # it will compare ascii values
+print("ABC"> "ACD") # it will compare first letter and then second letter and so on
+
+
+#Logical Operators
+# and, or, not
+print(123 > 100 and 34 == 34)
+print(12 != 12 or 23 == 45 or 67==56 or 10>5)
